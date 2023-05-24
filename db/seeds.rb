762d26bdda74f4c0b6cb6faab4954066b6f3a5c0
Roberto Barros <roberto.f.barros@gmail.com>
2023-05-24 18:12:18 -0300

    livecode

---
+++ db/seeds.rb
@@ -0,0 +1,3 @@
+Garden.create!(name: 'My garden', banner_url: 'https://images.unsplash.com/photo-1594498653385-d5172c532c00')
+Garden.create!(name: 'My other garden', banner_url: 'https://images.unsplash.com/photo-1578302758063-0ef3e048ca89')
+Garden.create!(name: 'Other more garden', banner_url: 'https://images.unsplash.com/photo-1623358519330-00f61d89396b')
